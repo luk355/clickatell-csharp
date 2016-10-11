12ed3c68839907b6e36054796c1d8a58507606af
luk355 <luk355@gmail.com>
2016-10-11 14:59:34 +1300

    Fixed sln project references

--- Build.ps1
+++ Build.ps1
@@ -59,6 +59,6 @@ Invoke-MSBuild
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-#exec { & dotnet test .\test\UnifiedMediatR.Tests -c Release }
+#exec { & dotnet test .\test\Clickatell.Services.Tests -c Release }
 
-exec { & dotnet pack .\src\UnifiedMediatR -c Release -o .\artifacts --version-suffix=$revision } 
+exec { & dotnet pack .\src\Clickatell.Services -c Release -o .\artifacts --version-suffix=$revision } 
