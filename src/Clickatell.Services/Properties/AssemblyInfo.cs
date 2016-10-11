48100e03863b27dbec379d9b64eff4656c69d5d0
luk355 <luk355@gmail.com>
2016-10-12 12:39:04 +1300

    Assembly info populated  - description, author etc.

--- src/Clickatell.Services/Properties/AssemblyInfo.cs
+++ src/Clickatell.Services/Properties/AssemblyInfo.cs
@@ -5,11 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Clickatell.Services")]
-[assembly: AssemblyDescription("Clickatell HTTP and REST clients")]
+[assembly: AssemblyDescription(@"Clickatell HTTP and REST clients providing basic SMS functionality.
+Supports sending messages, cancelling queued messages, getting message cost,status and account balance.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Lukas Behal")]
 [assembly: AssemblyProduct("Clickatell.Services")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.*")]
+[assembly: AssemblyFileVersion("0.0.2")]
