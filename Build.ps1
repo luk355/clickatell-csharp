7c3d913e073a5e12ff602904efb6721b3ecba5d6
luk355 <luk355@gmail.com>
2016-10-12 11:11:00 +1300

    Build adjustment - build as release configuration

--- Build.ps1
+++ Build.ps1
@@ -54,7 +54,7 @@ EnsurePsbuildInstalled
 
 exec { & nuget restore }
 
-Invoke-MSBuild
+Invoke-MSBuild -configuration Release
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
